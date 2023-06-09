0502afffabcca3a3fdd2c9986d6b05ed801b9676
lilitgrigoryan92 <lilittgrigoryan@gmail.com>
2023-06-09 10:20:52 -0700

    changed

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const fs=require("fs")
 function statistics(command,arguments=[],time=Infinity){
 
     const timestamp = new Date().toISOString()
-    const logFilePath = "logs/" + timestamp + command + ".json";
+    const logFilePath = "/logs" + timestamp + command + ".json";
+
 
     const start=new Date()
 
@@ -34,8 +35,8 @@ process.on("close",(code)=>{
         start:start.toString(),
         duration,
         success,
-        commandSuccess: success ? true : undefined,
-        error: success ? undefined : `Process exited with code ${code}`
+        commandSuccess: success ? {} : undefined,
+        error: success ? {} : console.log("Error message")
     }
 
 fs.writeFile(logFilePath,JSON.stringify(statistics,undefined,2),(err)=>{
